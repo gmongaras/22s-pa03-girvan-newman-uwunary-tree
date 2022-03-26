eeed5cc2c6657c3be28b2bf308b762e233fd8eb0
gmongaras <gabriel@mongaras.com>
2022-03-26 16:30:42 -0500

    Added Football Data

---
+++ data/gml_to_graphml.py
@@ -0,0 +1,16 @@
+from networkx import gml
+from networkx import graphml
+
+
+# Filename to convert
+inFile = "./football/football.gml"
+
+# Filename to write to
+outFile = inFile[:inFile.rfind(".")] + ".graphml"
+
+
+# Load in the graph
+G = gml.read_gml(inFile)
+
+# Convert the graph and save it
+graphml.write_graphml(G, outFile)
