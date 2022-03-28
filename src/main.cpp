6bf80496d6334cafc3da0e107e5c26e5fe0d2c9e
TrevorDohm <tdohm@smu.edu>
2022-03-28 11:22:51 -0500

    Boost Library Implemented Cmake (Im Not Sad)

--- src/main.cpp
+++ src/main.cpp
@@ -6,30 +6,51 @@
 #include <boost/graph/graphml.hpp>
 #include <boost/property_map/dynamic_property_map.hpp>
 #include <boost/property_map/property_map.hpp>
-
 #include <fstream>
 
-//struct GraphData { std::string Name; };
-//struct VertexProperty { std::string Name; };
-//struct EdgeProperty { std::string Name; };
-//
-//Graph ReadGraph(std::istream& in) {
-//
-//    Graph graph; // Creates Graph Named G
-//    boost::dynamic_properties dp; //(boost::ignore_other_properties); // Ignore Any Unmapped Extraneous Properties
-////    DP.property("Name", boost::get(&VertexProperty::Name, G));
-//    boost::read_graphml(in, graph, dp);
-//    return graph;
-//}
+struct GraphData { std::string Name; };
+struct VertexProperty { std::string Name; };
+struct EdgeProperty { std::string Name; };
+
+using Graph = boost::adjacency_list<boost::setS, boost::vecS, boost::directedS, VertexProperty, EdgeProperty, GraphData>;
+
+Graph ReadGraph(std::istream& is) {
+    Graph graph;
+    boost::dynamic_properties dp(boost::ignore_other_properties);
+    dp.property("Name", boost::get(&VertexProperty::Name, graph));
+
+    boost::read_graphml(is, graph, dp);
+
+    return graph;
+}
+
+extern std::string const graphml_111;
 
 int main() {
-
-    std::istringstream graphml("data/football/football.graphml");
-    typedef boost::adjacency_list<> Graph;
-    Graph graph;
-    boost::dynamic_properties dp;
-
-    boost::read_graphml(graphml, graph, dp);
-
-//    print_graph(graph, get(&VertexProperty::Name, graph));
+    std::istringstream is(graphml_111);
+    Graph g = ReadGraph(is);
+    print_graph(g, get(&VertexProperty::Name, g));
 }
+
+std::string const graphml_111 = R"(<?xml version="1.0" encoding="UTF-8"?>
+<graphml xmlns="http://graphml.graphdrawing.org/xmlns" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
+  <key id="key0" for="node" attr.name="Name" attr.type="string" />
+  <graph id="G" edgedefault="directed" parse.nodeids="canonical" parse.edgeids="canonical" parse.order="nodesfirst">
+    <node id="n0">
+      <data key="key0">A</data>
+    </node>
+    <node id="n1">
+      <data key="key0">D</data>
+    </node>
+    <node id="n2">
+      <data key="key0">B</data>
+    </node>
+    <node id="n3">
+      <data key="key0">C</data>
+    </node>
+    <edge id="e0" source="n0" target="n1">
+    </edge>
+    <edge id="e1" source="n2" target="n3">
+    </edge>
+  </graph>
+</graphml>)";
