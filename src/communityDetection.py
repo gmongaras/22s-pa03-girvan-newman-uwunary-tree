eb1c19187aa6e5a98d314f4b8eac764e1b4523ca
gmongaras <gabriel@mongaras.com>
2022-03-30 17:06:50 -0500

    Added Stopping Factor To GN

--- src/communityDetection.py
+++ src/communityDetection.py
@@ -4,14 +4,15 @@ from collections import deque
 import numpy as np
 import random
 from collections import deque
+import math
 
 
 
 
 
 # Parameters
-inFile = "../data/test.graphml"  # The datafile to load in
-nodeSubsetPercent = 0.9             # Number of random nodes to pick in the betweeness algorithm
+inFile = "../data/dataset.graphml"  # The datafile to load in
+nodeSubsetPercent = 0.8             # Number of random nodes to pick in the betweeness algorithm
 betThreshold = 4                    # Threshold betweeness value to remove
     
     
@@ -289,21 +290,86 @@ def main():
     # Read in the graph and store data on it
     G = graphml.read_graphml(inFile)
     
-    # Iterate until the number of edges is 0
+    # Iterate until the Q value is no longer increasing
+    Q_prev = -np.inf
+    Q = -np.inf
     maxBetweeness = [0]
-    while (len(G.edges) and len(maxBetweeness) > 0):
+    while (Q >= Q_prev):#len(G.edges) and len(maxBetweeness) > 0):
+        # Update the Q_prev value
+        Q_prev = Q
         
         # Calculate the betweeness of all edges in the graph
         betweeness = calculateBetweeness(G)
         
         # Get the edges with the max betweeness
-        maxBetweeness = np.argwhere(np.array(list(betweeness.values()), dtype=np.float16) >= 4)[:30]
+        #maxBetweeness = np.argwhere(np.array(list(betweeness.values()), dtype=np.float16) == list(betweeness.values())[np.argmax(np.array(list(betweeness.values()), dtype=np.float16))])
+        maxBetweeness = np.argwhere(np.array(list(betweeness.values()), dtype=np.float16) >= 3*math.log2(len(list(G.edges))))
+        
+        # Store the number of edges before removing any edges (m)
+        m = len(list(G.edges))
+        
+        # Store the old graph
+        oldG = G.copy()
         
         # Remove all max edges from the graph
         for edge in maxBetweeness:
             e = list(betweeness.keys())[edge.item()]
             G.remove_edge(e[0], e[1])
         
+        
+        
+        
+        
+        ### Compute the modularity (Q)
+        # https://www.pnas.org/doi/full/10.1073/pnas.0601602103#FD3
+        
+        # Compute the differences B_ij
+        # - B_ij = (A - (k_i-k_j)/2m)
+        #    - A = 1 if an egde is between node i and j
+        #           in the new graph
+        #    - k_i = degree of node i
+        #    - k_j = degree of node j
+        #    - m = number of edges in the old graph
+        # - s_i * s_j = 1 if i and j are in the same group, -1 otherwise
+        # - sum1 = The first summation
+        #    - sum_ij[ B_ij*(s_i*s_j +1) ]
+        # - sum2 = The second summation
+        #    - sum_ij[ B_ij ]
+        
+        # Iterate over all nodes in the old graph (i)
+        sum1 = 0
+        sum2 = 0
+        for i in list(oldG.nodes):
+            # Iterate over all nodes in the new graph (j)
+            for j in list(G.nodes):
+                # Calculate the B value
+                k_i = len(list(G.neighbors(i)))
+                k_j = len(list(G.neighbors(j)))
+                A = 1 if ((i, j) in G.edges or (j, i) in G.edges) else 0
+                
+                B = A - (k_i*k_j)/2*m
+                
+                
+                # Calculate the s value (s_i * s_j) + 1
+                comm = []
+                findCommunities(G, i, comm)
+                s = 1 if j in comm else -1
+                s += 1
+                
+                # Compute the B values to be summed
+                B_1 = B*s
+                B_2 = B
+                
+                # Store the B values
+                sum1 += B_1
+                sum2 += B_2
+        
+        # Compute the final Q value
+        Q = (1/2*m)*(0.5*sum1 - sum2)
+        
+        print()
+        
+    Q = oldG
         
     # Iterate over all nodes and find the communities
     comm = []           # The communities found
@@ -319,10 +385,16 @@ def main():
         # Add the visited nodes to the communities
         if len(visited) != 0:
             comm.append(visited)
+        else:
+            comm.append([node])
         
         # Add the visited nodes to the total visited nodes
         totalVisited += visited
-        
+    
+    c = nx.community.girvan_newman(G) 
+    k = 3   # number of communities
+    for _ in range(k-1):
+        comms = next(c)
     print()
 
 
