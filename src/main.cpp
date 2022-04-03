1d78f1c43967fa542d5c67ef2e5c1866dd207a14
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-04-03 14:59:50 -0500

    Working on GN

--- src/main.cpp
+++ src/main.cpp
@@ -9,39 +9,71 @@
 #include <boost/graph/graph_utility.hpp>
 #include <boost/graph/graphml.hpp>
 #include <boost/property_map/dynamic_property_map.hpp>
-#include <boost/property_map/property_map.hpp>
 
 #include <fstream>
 #include <Python.h>
+#include <random>
 #include <string>
+#include <map>
+#include <list>
 
-struct VertexProperty { long value; }; // Vertex ID (Community Number)
-using Graph = boost::adjacency_list<boost::setS, boost::vecS, boost::undirectedS, VertexProperty>;
-using vertex_descriptor = boost::graph_traits<Graph>::vertex_descriptor;
-
-Graph ReadGraph(std::ifstream& I) {
-    Graph G; // Creates Return Variable
-    boost::dynamic_properties D(boost::ignore_other_properties); // Dynamic Properties
-    D.property("value", boost::get(&VertexProperty::value, G)); // Vertex Community Getter
-    boost::read_graphml(I, G, D); // Read In Program Argument Graphml
-    return G; // Return Variable
-}
+// Adjacency List
+using Graph = boost::adjacency_list<boost::setS, boost::vecS, boost::undirectedS>;
 
 // Prints Important Information
 void PrintGraph(Graph const &G) {
     std::cout << "Vertex Amount: " << num_vertices(G) << "\n";
     std::cout << "Edge Amount: " << num_edges(G) << "\n";
-
-    // Prints All Connections From All Nodes (Using ID)
-    boost::print_graph(G, boost::get(&VertexProperty::value, G), std::cout);
-
-    // Prints All Edges
     auto es = boost::edges(G);
     for (auto eit = es.first; eit != es.second; ++eit) {
         std::cout << boost::source(*eit, G) << ' ' << boost::target(*eit, G) << std::endl;
     }
 }
 
+Graph ReadGraph(std::ifstream& I) {
+    Graph G; // Creates Return Variable
+    boost::dynamic_properties D(boost::ignore_other_properties); // Dynamic Properties
+    boost::read_graphml(I, G, D); // Read In Program Argument Graphml
+    return G; // Return Variable
+}
+
+// Shuffle List Contents
+template < typename T > void shuffle( std::list<T>& lst )
+{
+    // create a vector of (wrapped) references to elements in the list
+    // http://en.cppreference.com/w/cpp/utility/functional/reference_wrapper
+    std::vector< std::reference_wrapper< const T > > vec( lst.begin(), lst.end() ) ;
+
+    // shuffle (the references in) the vector
+    std::shuffle( vec.begin(), vec.end(), std::mt19937{ std::random_device{}() } ) ;
+
+    // copy the shuffled sequence into a new list
+    std::list<T> shuffled_list {  vec.begin(), vec.end() } ;
+
+    // swap the old list with the shuffled list
+    lst.swap(shuffled_list) ;
+}
+
+std::map<std::tuple<int, int>, float> calculateBetweenness(Graph const &G) {
+
+     std::map<std::tuple<int, int>, float> betweenness;
+     auto edges = G.m_edges;
+     shuffle(edges);
+}
+
+Graph normalLoop(Graph const &G) {
+
+    int Q_prev = INT_MIN;
+    int Q = INT_MIN;
+    int* maxBetweenness = new int[0];
+    int i = 1;
+
+    while (Q + 0.05 >= Q_prev) {
+        Q_prev = Q;
+        auto betweenness = calculateBetweenness(G);
+    }
+
+}
 
 // Handles Main Graph ( PrintGraph(G); )
 // Argument Example: data/football/football.graphml
@@ -50,22 +82,24 @@ int main(int argc, char* argv[]) {
         std::ifstream I(argv[1]);
         Graph G = ReadGraph(I);
 
-        boost::queue<vertex_descriptor> Q;
-        boost::default_bfs_visitor V;
+        G = normalLoop(G);
 
-        auto index_map = boost::get(boost::vertex_index, G);
-        auto color_map = boost::make_vector_property_map<boost::default_color_type>(index_map);
-
+//        boost::queue<vertex_descriptor> Q;
+//        boost::default_bfs_visitor V;
+//
+//        auto index_map = boost::get(boost::vertex_index, G);
+//        auto color_map = boost::make_vector_property_map<boost::default_color_type>(index_map);
+//
+////        for (auto vd : boost::make_iterator_range(vertices(G))) {
+////            std::cout << "vertex descriptor #" << vd
+////                      << " degree:" << degree(vd, G)
+////                      << " community:"     << G[vd].value
+////                      << "\n";
+////        }
+//
 //        for (auto vd : boost::make_iterator_range(vertices(G))) {
-//            std::cout << "vertex descriptor #" << vd
-//                      << " degree:" << degree(vd, G)
-//                      << " community:"     << G[vd].value
-//                      << "\n";
+//            boost::breadth_first_search(G, vd, Q, V, color_map); // Time Complexity: O(E + V)
 //        }
-
-        for (auto vd : boost::make_iterator_range(vertices(G))) {
-            boost::breadth_first_search(G, vd, Q, V, color_map); // Time Complexity: O(E + V)
-        }
     }
     else {
         Py_Initialize(); // Initialize Environment
